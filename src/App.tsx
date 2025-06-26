2ee4234d708fd74db859f64dccc6499f70bf2e69
Stella-055 <stellaongayo243@gmail.com>
2025-06-26 17:23:22 +0300

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@mui/material'
+import { IoMdPlayCircle } from "react-icons/io";
+import { useReducer } from 'react';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  type playingstate = {
+    startbutton: string;
+    guessbutton: boolean;
+    input: number;
+    feedback: string | null;
+    message: string;
+  };
 
+  type actionType = { type: string
+    payload:number
+  }
+   
+  const initialState: playingstate = 
+  {
+    startbutton: "",
+    guessbutton: true,
+    input: 2,
+    feedback: null,
+    message: "Welcome Player",
+  }
+  const [state, dispatch] = useReducer(reducerFunction, initialState);
+
+function reducerFunction(state: playingstate, action: actionType) {
+
+  switch (action.type) {
+    case "START":
+      return { ...state, startbutton: "PLAYING", message: "Game Started" };
+    case "GUESS":
+     
+    default:
+      return state;
+  }
+
+}
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+    <div className='navdiv'>
+ <Button variant="contained" size='large' endIcon={<IoMdPlayCircle />}>
+  PLAY
+</Button>
+      
+</div>
+<div className='maindiv'>
+<h1>Welcome Player</h1>
+<input type="number"  />
+{state.feedback && <h2>{state.feedback}</h2>}
+
+<button  className='guessbutton' disabled= {state.guessbutton} >
+Guess
+</button>
+
+</div>
+    </div>
   )
 }
 
