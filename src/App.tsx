4ee048672db55a0f3287145ac7b81ebd20193160
Stella-055 <stellaongayo243@gmail.com>
2025-06-26 19:46:57 +0300

    functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,58 @@
 import './App.css'
 import { Button } from '@mui/material'
 import { IoMdPlayCircle } from "react-icons/io";
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 
 function getrandomNumber() : number {
   return Math.floor(Math.random() * 100) + 1;
 }
 
 function App() {
+  const [playerguess, setPlayerguess] = useState<number | null>(null);
   type playingstate = {
     startbutton: boolean;
     guessbutton: boolean;
-    input: number;
-    feedback: string | null;
+   feedback: string | null;
     message: string;
    inputstatedisabled: boolean;
-   secretnumber?: number;
+   secretnumber: number;
+   numberoftrials: number;
   };
 
   type actionType = { type: string
-    payload?:number
+    payload?:number | null;  
   }
    
   const initialState: playingstate = 
   {
     startbutton: false,
     guessbutton: true,
-    input: 2,
+   secretnumber: getrandomNumber(),
     feedback: null,
     message: "Welcome Player Guess a number between 1 and 100",
     inputstatedisabled: true,
-   
+    numberoftrials: 10
   }
   const [state, dispatch] = useReducer(reducerFunction, initialState);
 
-function reducerFunction(state: playingstate, action: actionType) {
-
+function reducerFunction(state:playingstate, action:actionType) {
+ 
   switch (action.type) {
     case "START":
-      return { ...state, startbutton: true, message: "10 chances left" , guessbutton: false,inputstatedisabled: false ,secretnumber: getrandomNumber()};
+      return { ...state, startbutton: true, message: "10 chances left" , guessbutton: false,inputstatedisabled: false ,secretnumber: getrandomNumber(), numberoftrials: 10};
     case "GUESS":
+      state = { ...state, numberoftrials: state.numberoftrials - 1 };
+      if( state.numberoftrials ==0) {
+        return { ...state, feedback: `${state.numberoftrials} chances left! Please start a new game.`, guessbutton: true, inputstatedisabled: true, message: `Your score is ${state.numberoftrials *100}%` };
+      }
+      else if(state.secretnumber === action.payload) {
+        return { ...state, feedback: ` ${state.numberoftrials} Chances left.Congratulations! You guessed the number!`, guessbutton: true, inputstatedisabled: true, message: ` Your score is ${state.numberoftrials }%`};}
+    else if ( action.payload && state.secretnumber <= action.payload) {
+    return { ...state, feedback: "Your guess is too high!",  message: `You have ${state.numberoftrials} chances left` };
+        }
+        else if (action.payload && state.secretnumber >= action.payload) {
+          return { ...state, feedback: "Your guess is too low!", message: `You have ${state.numberoftrials} chances left` };
+        }
      
     default:
       return state;
@@ -56,10 +69,12 @@ function reducerFunction(state: playingstate, action: actionType) {
 </div>
 <div className='maindiv'>
 <h1>{state.message}</h1>
-<input type="number" readOnly={state.inputstatedisabled} />
+<input type="number" readOnly={state.inputstatedisabled} onChange={(e: React.ChangeEvent<HTMLInputElement>)=>{
+  setPlayerguess(Number(e.target.value));
+}} />
 {state.feedback && <h2>{state.feedback}</h2>}
 
-<Button  variant='outlined' disabled= {state.guessbutton}onClick={() => dispatch({ type: "GUESS"})} >
+<Button  variant='outlined' disabled= {state.guessbutton}onClick={() => dispatch({ type: "GUESS",payload:playerguess})} >
 Guess
 </Button>
 
